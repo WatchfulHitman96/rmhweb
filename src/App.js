34a2c33433b0129b156bf5e8bd6272e5842fca52
WatchfulHitman96 <akashnil64@gmail.com>
2025-11-25 09:01:41 +0530

    added new photographs to success stories Tandra Das and Soumyadip Guin

--- src/App.js
+++ src/App.js
@@ -198,11 +198,13 @@ const SubjectsPage = () => {
 // --- Success Stories Page Component ---
 const SuccessStoriesPage = () => {
     const stories = [
-        { name: "Bidhidiptya Mondal", img: "/img/success_stories/bidhidiptya-mondal.jpg" }, // Assuming local image
-        { name: "Debjit Ghosh", img: "/img/success_stories/debjit-ghosh.jpg" },       // Assuming local image
-        { name: "Toppers", img: "/img/success_stories/toppers.jpg" },             // Assuming local image
+        { name: "Bidhidiptya Mondal", img: "/img/success_stories/bidhidiptya-mondal.jpg" }, 
+        { name: "Debjit Ghosh", img: "/img/success_stories/debjit-ghosh.jpg" },       
+        { name: "Toppers", img: "/img/success_stories/toppers.jpg" },             
         { name: "Soumyadip Guin", img: "/img/success_stories/Soumyadip_guin.jpg"},
-        { name: "Kohima Roy", img: "/img/success_stories/kohima_roy.jpg"}
+        { name: "Kohima Roy", img: "/img/success_stories/kohima_roy.jpg"},
+        { name: "Soumyadip Guin new", img: "/img/success_stories/Soumyadip_guin_new.jpg"},
+        { name: "Tandrima Das", img: "/img/success_stories/Tandrima_Das.jpg"}
     ];
     return (
         <div className="p-8 md:p-12 flex-1 flex flex-col">
